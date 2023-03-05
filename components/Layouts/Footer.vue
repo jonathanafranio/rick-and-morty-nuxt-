e3dc960c39861c35ed55ab8ddffbc6de0d2ac226
jonathanafranio <jonathan.afranio.s@gmail.com>
2023-03-05 20:47:14 -0300

    target blank no link no footer

--- components/Layouts/Footer.vue
+++ components/Layouts/Footer.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
     footer.footer
         .container
-            p © Copyright 2023. Desenvolvido por <a href="https://jonathanafranio.github.io/cv/">Jonathan Afranio</a>. Todos os direitos reservados.
+            p © Copyright 2023. Desenvolvido por <a href="https://jonathanafranio.github.io/cv/" target="_blank">Jonathan Afranio</a>. Todos os direitos reservados.
 </template>
 
 <script>
