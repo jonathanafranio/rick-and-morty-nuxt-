62d3dc2deb6a5e971fe9220ee26f63e5c1a91932
jonathanafranio <jonathan.afranio.s@gmail.com>
2023-03-05 20:23:51 -0300

    feat: rodapé

---
+++ components/Layouts/Footer.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+    footer.footer
+        .container
+            p © Copyright 2023. Desenvolvido por <a href="https://jonathanafranio.github.io/cv/">Jonathan Afranio</a>. Todos os direitos reservados.
+</template>
+
+<script>
+    export default {
+        name: 'Footer'
+    }
+</script>
--- components/Layouts/LayoutDefault.vue
+++ components/Layouts/LayoutDefault.vue
@@ -4,15 +4,18 @@
         main.container
             section.page-section
                 <slot />
+        Footer
 
 </template>
 <script>
 import Header from '@/components/Layouts/Header'
+import Footer from '@/components/Layouts/Footer.vue'
 
 export default {
     name: 'LayoutDefault',
     components: {
-        Header
+        Header,
+        Footer
     },
     methods: {
     },
